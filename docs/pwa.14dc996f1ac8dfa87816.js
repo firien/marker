2f57ae4b5561885afa7a5e8aa28211d7e4d7c225
Patrick Griffin <halifirien@gmail.com>
2019-01-29 22:52:23 -0500

    forgot to build in production mode

--- docs/pwa.14dc996f1ac8dfa87816.js
+++ docs/pwa.14dc996f1ac8dfa87816.js
@@ -1,7 +1,7 @@
 (function() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service.js', {
-      scope: '/'
+    navigator.serviceWorker.register('/marker/service.js', {
+      scope: '/marker/'
     }).then(function(registration) {
       var refreshPage;
       refreshPage = function(worker) {
